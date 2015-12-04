4fffa60f09dbb64bfee205410c672fa9ba5e2e1a
Thiago Rose <thiago.senhorinha@gmail.com>
2015-12-03 23:40:45 -0200

    Implement AnagramChecker.check method

--- app/models/anagram_checker.rb
+++ app/models/anagram_checker.rb
@@ -1,6 +1,16 @@
 class AnagramChecker
 
   def self.check(textOne, textTwo)
+    prepare_to_compare(textOne) === prepare_to_compare(textTwo)
+  end
+
+  private
+  def self.prepare_to_compare(text)
+    remove_all_whitespaces(text).downcase.chars.sort.join
+  end
+
+  def self.remove_all_whitespaces(text)
+    text.gsub(/\s+/, "")
   end
 
 end
